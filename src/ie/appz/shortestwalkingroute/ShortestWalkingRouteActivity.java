473a8f8bba209210553f680d3e7c1febd6caa458
Rory Glynn <ankhwatcher@gmail.com>
2012-02-05 15:08:23 +0000

    Version 0.0.2 http://shortestwalkingroute.appz.ie/?p=22

--- src/ie/appz/shortestwalkingroute/ShortestWalkingRouteActivity.java
+++ src/ie/appz/shortestwalkingroute/ShortestWalkingRouteActivity.java
@@ -6,6 +6,10 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
 import android.widget.TextView;
 /* Rory Glynn DT081/4
  * Implements location information gathering from http://developer.android.com/guide/topics/location/obtaining-user-location.html
@@ -13,17 +17,28 @@ import android.widget.TextView;
  * */
 public class ShortestWalkingRouteActivity extends Activity
 {
+	String networkLocation;
+	Long startMillis;
+	LocationListener gpsListener, networkListener;
+	LocationManager locationManager;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
      // Acquire a reference to the system Location Manager
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        
+        //Start Count
+        Time startTime = new Time();
+        startTime.setToNow();
+        startMillis = startTime.toMillis(false);
+        
         // Define a listener that responds to location updates
-        LocationListener networkListener = new LocationListener() {
+        networkListener = new LocationListener() {
             public void onLocationChanged(Location location)
             {
               // Called when a new location is found by the network location provider.
@@ -36,7 +51,7 @@ public class ShortestWalkingRouteActivity extends Activity
 
             public void onProviderDisabled(String provider) {}
           };
-          LocationListener gpsListener = new LocationListener() {
+          gpsListener = new LocationListener() {
               public void onLocationChanged(Location location)
               {
                 // Called when a new location is found by the network location provider.
@@ -58,21 +73,42 @@ public class ShortestWalkingRouteActivity extends Activity
 
 	protected void makeUseOfNetwork(Location location)
 	{
-		TextView networkDisplay =new TextView(this); 
-		networkDisplay=(TextView)findViewById(R.id.network_display);
-		
+		/*TextView networkDisplay =new TextView(this); 
+		networkDisplay=(TextView)findViewById(R.id.network_display);*/
+		//Convert the location's longitude and latitude properties into strings
 		String latitude = new Double(location.getLatitude()).toString();
 		String longitude = new Double(location.getLongitude()).toString();
-		networkDisplay.setText(latitude +","+ longitude);
-		/*networkDisplay.setText(location.toString());*/
+		
+		//Combine the values into a single String to be displayed when the GPS location next updates
+		networkLocation = latitude +","+ longitude;
+		/*networkDisplay.setText(latitude +","+ longitude);
+		networkDisplay.setText(location.toString());*/
 		
 	}
 
 	protected void makeUseOfGPS(Location location)
 	{
-		//Grab the gps_display TextView
-		TextView gpsDisplay =new TextView(this); 
-		gpsDisplay=(TextView)findViewById(R.id.gps_display);
+		Time currentTime = new Time();
+        currentTime.setToNow();
+        long currentMillis = currentTime.toMillis(false);
+        long runningMillis = currentMillis - startMillis;
+        
+		//Grab the main_table
+		TableLayout mainTable = (TableLayout)findViewById(R.id.main_table);
+		//Create a new TableRow
+		TableRow dynamicRow = new TableRow(this);
+        dynamicRow.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
+        //dynamicRow.setBackgroundResource(R.color.solid_black);
+		//create the TextViews to contain the new values
+        TextView networkDisplay = new TextView(this);
+		networkDisplay.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+		networkDisplay.setTextColor(0xFF0000FF);
+		TextView cellDivider = new TextView(this);
+		cellDivider.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
+		cellDivider.setTextColor(0xFF000000);
+		TextView gpsDisplay =new TextView(this);
+		gpsDisplay.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
+		gpsDisplay.setTextColor(0xFFFF0000);
 		
 		//Convert the location's longitude and latitude properties into strings
 		String latitude = new Double(location.getLatitude()).toString();
@@ -80,6 +116,23 @@ public class ShortestWalkingRouteActivity extends Activity
 		
 		//Display the longitude and latitude on screen
 		gpsDisplay.setText(latitude +","+ longitude);
+		cellDivider.setText(R.string.pipe_divider);
+		networkDisplay.setText(networkLocation);
 		
+		//Add both TextViews to the dynamicRow
+		dynamicRow.addView(networkDisplay, 0);
+		dynamicRow.addView(cellDivider,1);
+		dynamicRow.addView(gpsDisplay, 2);
+		
+		/* Add row to TableLayout. */
+        mainTable.addView(dynamicRow,new TableLayout.LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
+        
+        
+        if(runningMillis > 300000)
+        {
+        	// Kill location Listeners
+        	locationManager.removeUpdates(networkListener);
+        	locationManager.removeUpdates(gpsListener);
+        }
 	}
 }
